f79aa167236ad12e06b85d18736bb86ea115a536
alunfes <ts4792@gmail.com>
2019-05-17 09:47:28 +0900

    19May

--- WebsocketMaster.py
+++ WebsocketMaster.py
@@ -28,7 +28,7 @@ class WebsocketMaster:
             on_open = self.on_open, on_message = self.on_message,
             on_error = self.on_error, on_close = self.on_close)
         #self.ws.keep_running = False
-        websocket.enableTrace(False)
+        websocket.enableTrace(True)
         #self.thread = threading.Thread(target=lambda: self.ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE}))
         self.thread = threading.Thread(target=lambda: self.ws.run_forever())
         self.thread.daemon = True
